28f9cfb22c01e4e79da173d39db84cfa9b68198f
dblarremore <daniel.larremore@colorado.edu>
2019-10-09 09:32:52 -0600

    added Robert Steele

--- images/people/edit/gifsicle.sh
+++ images/people/edit/gifsicle.sh
@@ -1,5 +1,6 @@
 gifsicle -U input.gif "#-2-1" -o reverse.gif
 gifsicle -d 11 input.gif reverse.gif > boomerang.gif
 gifsicle -U boomerang.gif `seq -f "#%g" 0 3 999` -O2 -o output.gif
+# gifsicle -U --flip-horizontal mike.gif > mike2.gif
 rm boomerang.gif
 rm reverse.gif
