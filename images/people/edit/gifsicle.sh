ba21e4416af30abc0c67dfb9c5a4ba06be4452ce
dblarremore <daniel.larremore@colorado.edu>
2019-10-10 11:37:50 -0600

    aaron aaeng

--- images/people/edit/gifsicle.sh
+++ images/people/edit/gifsicle.sh
@@ -1,5 +1,5 @@
 gifsicle -U input.gif "#-2-1" -o reverse.gif
-gifsicle -d 11 input.gif reverse.gif > boomerang.gif
+gifsicle -d 9 input.gif reverse.gif > boomerang.gif
 gifsicle -U boomerang.gif `seq -f "#%g" 0 3 999` -O2 -o output.gif
 # gifsicle -U --flip-horizontal mike.gif > mike2.gif
 rm boomerang.gif
